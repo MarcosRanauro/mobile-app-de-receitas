efdf2cb011f4f6a41d83da65fb384e241b5eafed
MarcosRanauro <marcosranauro@hotmail.com>
2024-06-16 23:37:58 -0300

    Arrumando erros de dependencias e gerando apk

--- app-de-receitas/src/environments/environment.prod.ts
+++ app-de-receitas/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCiFVKJZCKkC5VRAtrYb1KI5ZFepJ5XUSk",
+    authDomain: "app-de-receitas-mobile.firebaseapp.com",
+    projectId: "app-de-receitas-mobile",
+    storageBucket: "app-de-receitas-mobile.appspot.com",
+    messagingSenderId: "754120962301",
+    appId: "1:754120962301:web:9b849d3882e4576e0b0e94",
+    measurementId: "G-7Q2YTL1NF9"
+  }
 };
