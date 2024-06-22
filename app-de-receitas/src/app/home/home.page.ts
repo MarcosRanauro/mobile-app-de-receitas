b162def25319671c9db27b34964572ad9d3c1d1c
MarcosRanauro <marcosranauro@hotmail.com>
2024-06-22 19:17:35 -0300

    Arrumando algumas escritas e resolvendo problemas de redirecionamento

--- app-de-receitas/src/app/home/home.page.ts
+++ app-de-receitas/src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +12,26 @@ import { NavController } from '@ionic/angular';
 export class HomePage {
   receitas: any[] = [];
   dataReturned: any;
+  user: any;
 
-  constructor(private apiService: ApiService, private navCtrl: NavController) {}
+  constructor(
+    private apiService: ApiService,
+    private navCtrl: NavController,
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.buscarCategorias();
+    this.loadUserProfile();
+  }
+
+  async loadUserProfile() {
+    const currentUser = await this.authService.getCurrentUser();
+    if(currentUser) {
+      this.user = await this.userService.getUserData(currentUser.uid);
+      console.log('User:', this.user);
+    }
   }
 
   buscarCategorias() {
